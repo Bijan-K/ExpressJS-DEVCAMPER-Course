cf86de365e24f1a50f1762552605e9df60a2aa8a
Bijan k <80640045+Bijan-K@users.noreply.github.com>
2023-03-03 14:22:52 +0330

    mongoose and database

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env
 dotenv.config({ path: './config/config.env' });
+
+//connectdb
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -18,4 +23,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in`));
+const server = app.listen(PORT, console.log(`server running in`));
+
+//handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`error: ${err.message}`);
+
+  //close server
+  server.close(() => process.exit(1));
+});
